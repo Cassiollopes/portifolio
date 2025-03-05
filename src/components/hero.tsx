e1940e6c3f11bc0962aa569a2b2ed30a10d135fc
cassio <cassio8976@gmail.com>
2025-03-05 19:44:10 -0300

    modified:   src/components/hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -23,7 +23,7 @@ const links = [
 export default function Hero() {
   return (
     <div className="flex flex-col gap-10 py-16 max-md:py-12 justify-center w-full md:items-center px-8">
-      <div className="md:text-center flex flex-col gap-5 font-bold md:items-center">
+      <div className="md:text-center flex flex-col gap-5 font-semibold md:items-center">
         <div className="flex items-center gap-2">
           <Image
             src="https://github.com/cassiollopes.png"
@@ -40,16 +40,15 @@ export default function Hero() {
         </h1>
       </div>
       <p className="md:text-center max-w-[500px] font-normal">
-        Tenho 19 anos, estou no terceiro semestre de faculdade e procuro minha
-        primeira oportunidade de estagio na área de desenvolvimento de software.
+        Tenho 19 anos, estou no terceiro semestre de faculdade e procuro uma oportunidade de estágio na área de desenvolvimento de software.
       </p>
-      <div className="flex gap-3 items-center justify-center max-md:flex-col">
+      <div className="flex gap-2 items-center justify-center max-md:flex-col">
         {links.map((link, i) => (
           <Link
             key={i}
             href={link.link}
             target="_blank"
-            className="gap-1 p-2 px-4 border rounded-sm text-white/50 hover:text-white transition-all duration-200 ease-in-out flex items-center bg-white/[0.02] hover:bg-white/10 max-md:w-full justify-center text-base"
+            className="gap-1 p-2 px-4 border rounded-sm text-white/50 hover:text-white transition-all duration-200 ease-in-out flex items-center bg-white/[0.02] hover:bg-white/10 max-md:w-full justify-center text-base md:w-[125px]"
           >
             <div>{link.icon}</div>
             {link.name}
